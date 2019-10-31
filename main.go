e8072eaa2189f614091b680d68b3d10d7e4349cc
kei <kei.adc11.29@gmail.com>
2019-10-31 17:30:21 +0900

    create room

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/ophum/hackathon20191031/message"
 	"github.com/gin-gonic/gin"
 	"github.com/ophum/hackathon20191031/room"
+	"strconv"
 )
 
 var rooms []room.Room
@@ -23,10 +25,21 @@ func CreateRoom(c *gin.Context) {
 	id := len(rooms) + 1
 
 	room := room.NewRoom(id, name)
+	m := message.NewMessage(1, "hello", "20291")
+	room.Messages = append(room.Messages, *m)
 	rooms = append(rooms, *room)
 
 	c.Redirect(301, "/")
 }
+
+func RoomIndex(c *gin.Context)  {
+	id := c.Param("id")
+	i, _ := strconv.ParseInt(id,10,32)
+	room := rooms[i-1]
+	c.HTML(200, "room.html", gin.H{"room": room})
+}
+
+
 
 func main() {
 	r := gin.Default()
@@ -34,9 +47,9 @@ func main() {
 	r.Use(sampleMiddleware())
 
 	r.GET("/", Index)
-
+	r.GET("/room/:id", RoomIndex)
 	r.POST("/room", CreateRoom)
 
-	r.Run()
+	r.Run(":8000")
 
 }
